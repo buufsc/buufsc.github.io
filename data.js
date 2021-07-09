c6b49b1f4b6b79a8c0c4126e08fc48bcfaec6c24
buufsc <87207061+buufsc@users.noreply.github.com>
2021-07-09 17:30:05 -0300

    Primeiro commit

---
+++ data.js
@@ -0,0 +1,787 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrada",
+      "name": "Entrada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.661675723554719,
+        "pitch": 0,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3949289611475475,
+          "pitch": 0.24800774179898966,
+          "rotation": 0,
+          "target": "1-hall-de-entrada"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.8698333778516982,
+          "pitch": 0.295807818331113,
+          "title": "Crédito da imagem",
+          "text": "Luis Hioka"
+        },
+        {
+          "yaw": 1.4492112180252779,
+          "pitch": 0.13609557270853756,
+          "title": "Reitoria",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "1-hall-de-entrada",
+      "name": "Hall de entrada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.11276285780843409,
+        "pitch": 0,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.004619067160854584,
+          "pitch": 0.19306453024016434,
+          "rotation": 0,
+          "target": "2-acesso-ao-1o-andar"
+        },
+        {
+          "yaw": -0.4358533041433681,
+          "pitch": -0.2584427121965085,
+          "rotation": 0,
+          "target": "3-hall---1o-andar"
+        },
+        {
+          "yaw": 2.918493700064478,
+          "pitch": 0.08233735054127322,
+          "rotation": 0,
+          "target": "0-entrada"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-acesso-ao-1o-andar",
+      "name": "Acesso ao 1o. andar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.06525332970010744,
+        "pitch": 0,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.256006203643226,
+          "pitch": 0.12919531950728924,
+          "rotation": 0,
+          "target": "1-hall-de-entrada"
+        },
+        {
+          "yaw": 1.3966176291363137,
+          "pitch": 0.3475554129841427,
+          "rotation": 0,
+          "target": "3-hall---1o-andar"
+        },
+        {
+          "yaw": -1.5933912996138648,
+          "pitch": 0.47492996865012316,
+          "rotation": 0,
+          "target": "11-entrada---trreo"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.23574229465219432,
+          "pitch": 0.052253307428918205,
+          "title": "Armários",
+          "text": ""
+        },
+        {
+          "yaw": 2.5666885333891134,
+          "pitch": -0.13274137080708925,
+          "title": "Reprografia",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "3-hall---1o-andar",
+      "name": "Hall - 1o. andar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.720814359320979,
+        "pitch": 0,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0199740686575947,
+          "pitch": 0.2695178433221095,
+          "rotation": 0,
+          "target": "2-acesso-ao-1o-andar"
+        },
+        {
+          "yaw": 2.8039188519130773,
+          "pitch": 0.3080330327430083,
+          "rotation": 0,
+          "target": "1-hall-de-entrada"
+        },
+        {
+          "yaw": -1.3239692782908818,
+          "pitch": 0.34419076081931976,
+          "rotation": 0,
+          "target": "4-referncia"
+        },
+        {
+          "yaw": -0.2190758899226921,
+          "pitch": 0.44443192052613156,
+          "rotation": 0,
+          "target": "10-novas-aquisies-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.9874130787703201,
+          "pitch": -0.349259669387493,
+          "title": "Crédito da imagem",
+          "text": "Júlio Cancellier"
+        },
+        {
+          "yaw": 2.6076880265622515,
+          "pitch": 0.07027511026238109,
+          "title": "Auditório Helke Hering",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "4-referncia",
+      "name": "Referência",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.45636997331359,
+        "pitch": 0,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5231644102987829,
+          "pitch": 0.14986052438956143,
+          "rotation": 0,
+          "target": "10-novas-aquisies-"
+        },
+        {
+          "yaw": 0.22656542268613933,
+          "pitch": 0.08752234530900616,
+          "rotation": 0,
+          "target": "3-hall---1o-andar"
+        },
+        {
+          "yaw": 2.845417247191657,
+          "pitch": 0.13907823668992947,
+          "rotation": 0,
+          "target": "5-sirius"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-sirius",
+      "name": "Sirius",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.183716653120322,
+        "pitch": 0,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08761657783815124,
+          "pitch": 0.10145573974487121,
+          "rotation": 0,
+          "target": "10-novas-aquisies-"
+        },
+        {
+          "yaw": -1.7084788856476703,
+          "pitch": 0.14281951055708753,
+          "rotation": 0,
+          "target": "6-vega"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-vega",
+      "name": "Vega",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.44686797990179,
+        "pitch": 0,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.858181775167548,
+          "pitch": 0.016210959471688824,
+          "rotation": 0,
+          "target": "5-sirius"
+        },
+        {
+          "yaw": -2.6843221555524472,
+          "pitch": 0.20973860116067478,
+          "rotation": 10.210176124166829,
+          "target": "14-armazm"
+        },
+        {
+          "yaw": 2.755538553801907,
+          "pitch": 0.1060675739523056,
+          "rotation": 4.71238898038469,
+          "target": "7-bellatrix"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-bellatrix",
+      "name": "Bellatrix",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.1156934841917554,
+        "pitch": 0,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.824415311691876,
+          "pitch": 0.06894331010310317,
+          "rotation": 0,
+          "target": "6-vega"
+        },
+        {
+          "yaw": -1.5394680280389572,
+          "pitch": 0.04945462454160676,
+          "rotation": 4.71238898038469,
+          "target": "5-sirius"
+        },
+        {
+          "yaw": 1.5658591806586406,
+          "pitch": 0.051830833647212415,
+          "rotation": 0,
+          "target": "8-colees-especiais"
+        },
+        {
+          "yaw": 1.9865240610025339,
+          "pitch": 0.029690316920145676,
+          "rotation": 1.5707963267948966,
+          "target": "9-balco-de-emprstimo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-colees-especiais",
+      "name": "Coleções especiais",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.4375732591461894,
+        "pitch": 0,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6238129085353474,
+          "pitch": 0.05862552597975146,
+          "rotation": 0,
+          "target": "7-bellatrix"
+        },
+        {
+          "yaw": -0.5062113799310346,
+          "pitch": 0.3653054572562606,
+          "rotation": 0,
+          "target": "13-peridicos"
+        },
+        {
+          "yaw": -1.691924257119327,
+          "pitch": 0.13579832314882978,
+          "rotation": 0,
+          "target": "9-balco-de-emprstimo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-balco-de-emprstimo",
+      "name": "Balcão de empréstimo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.1334053653009413,
+        "pitch": 0.0006547810121162456,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.691168800645829,
+          "pitch": 0.13110266845727736,
+          "rotation": 0,
+          "target": "8-colees-especiais"
+        },
+        {
+          "yaw": -1.4246301070213274,
+          "pitch": 0.13265456378641716,
+          "rotation": 0,
+          "target": "10-novas-aquisies-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-novas-aquisies-",
+      "name": "Novas aquisições ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7871651431624382,
+          "pitch": 0.10955409992523357,
+          "rotation": 0,
+          "target": "5-sirius"
+        },
+        {
+          "yaw": 0.06816025772651635,
+          "pitch": 0.15718490116137573,
+          "rotation": 0,
+          "target": "9-balco-de-emprstimo"
+        },
+        {
+          "yaw": 3.0930622258624227,
+          "pitch": 0.2704648296265333,
+          "rotation": 0,
+          "target": "3-hall---1o-andar"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-entrada---trreo",
+      "name": "Entrada - térreo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.319351881069963,
+        "pitch": 0,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7753876099310286,
+          "pitch": 0.2642931557278807,
+          "rotation": 0,
+          "target": "2-acesso-ao-1o-andar"
+        },
+        {
+          "yaw": -1.4157952977674597,
+          "pitch": 0.11190134334534463,
+          "rotation": 0,
+          "target": "12-sala-de-estudo-individual"
+        },
+        {
+          "yaw": 0.284311358342503,
+          "pitch": 0.18754469094369597,
+          "rotation": 0,
+          "target": "13-peridicos"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.2518487331437917,
+          "pitch": 0.10869157734627244,
+          "title": "Reprografia",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "12-sala-de-estudo-individual",
+      "name": "Sala de estudo individual",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.6671053657443187,
+        "pitch": 0,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5364103570905714,
+          "pitch": 0.1664683987874298,
+          "rotation": 0,
+          "target": "11-entrada---trreo"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.0674557654212187,
+          "pitch": 0.07950100214501532,
+          "title": "Sala de estudo individual",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "13-peridicos",
+      "name": "Periódicos",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.2853722880238045,
+        "pitch": 0,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0248093761787374,
+          "pitch": 0.10655986637971004,
+          "rotation": 0,
+          "target": "11-entrada---trreo"
+        },
+        {
+          "yaw": -0.22890485241090985,
+          "pitch": 0.15088206892521505,
+          "rotation": 5.497787143782138,
+          "target": "8-colees-especiais"
+        },
+        {
+          "yaw": -0.06179094853946232,
+          "pitch": 0.06378996334633058,
+          "rotation": 4.71238898038469,
+          "target": "14-armazm"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-armazm",
+      "name": "Armazém",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.3866800866834996,
+        "pitch": 0,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5595233935648523,
+          "pitch": 0.21513546258547933,
+          "rotation": 11.780972450961727,
+          "target": "13-peridicos"
+        },
+        {
+          "yaw": -2.0802361387241124,
+          "pitch": 0.1055971075767701,
+          "rotation": 0,
+          "target": "6-vega"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.6650977819288855,
+          "pitch": 0.002231250175700694,
+          "title": "&nbsp;Laboratório de Aprendizagem BU/CIN",
+          "text": ""
+        }
+      ]
+    }
+  ],
+  "name": "BU/UFSC - Visita orientada virtual",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
